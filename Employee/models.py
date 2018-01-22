3e87004e3f5f4c2b91e447c16bbd12c876c9060f
Haidar Zeineddine <HaidarZ1994@gmail.com>
2018-01-22 23:48:10 +0200

    Code refactoring

--- Employee/models.py
+++ Employee/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
 class Vacation(models.Model):
-    employee = models.ForeignKey()
-    description=models.CharField()
+    employee = models.ForeignKey(User)
+    description = models.CharField()
     fromdate = models.DateField()
